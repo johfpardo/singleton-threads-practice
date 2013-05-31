15bf923f7e61a2148cab0cde867db74b14e569b9
ajalvareze <ajalvareze@unal.edu.co>
2013-05-31 15:00:00 -0500

    create project

---
+++ SingletonThreads/src/singletonthreads/ProcessOne.java
@@ -0,0 +1,19 @@
+package singletonthreads;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Antonio
+ */
+public class ProcessOne implements Runnable{
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
